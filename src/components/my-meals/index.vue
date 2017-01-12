3b1bf7d1eb5716407a1564a20424f5ff005debaf
Mark Fridman <marky.fridman@gmail.com>
2017-01-12 13:34:10 +0200

    module update

---
+++ src/components/my-meals/index.vue
@@ -0,0 +1,5 @@
+<template src="./my-meals.component.html"></template>
+<script src="./my-meals.component.js"></script>
+<style src="./my-meals.component.scss" scoped lang="scss"></style>
+
+
---
+++ src/components/my-meals/my-meals.component.js
@@ -0,0 +1,30 @@
+export default {
+    data: () => {
+        return {
+            meals: [{
+                        _id: "58773fb3036f4b25abe93b9c",
+                        foods: [
+                            "Banana",
+                            "Apple"
+                        ],
+                        time: 1484210059985,
+                        userId: "58773eec036f4b25abe93b03"
+                    },
+                    {
+                        _id: "58773eb3036f4b25abe93b9c",
+                        foods: [
+                            "Pasta",
+                            "Tortilla"
+                        ],
+                        time: 1484710359985,
+                        userId: "58773fec036f4b25abe93b03"
+                    }]
+        }
+    },
+    methods: {
+
+    },
+    mounted() {
+        console.log('mashu');
+    }
+}
