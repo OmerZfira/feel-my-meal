55ec6a98881df4fe655bcf8e5e3c538c65a3af31
Mark Fridman <marky.fridman@gmail.com>
2017-01-14 13:45:04 +0200

    events

--- src/components/my-meals/my-meals.component.js
+++ src/components/my-meals/my-meals.component.js
@@ -4,20 +4,40 @@ import FullCalendar from 'fullcalendar';
 export default {
     data: () => {
         return {
-            
-            meals: [{
-                id: "1",
-                title: "Banana, Apple",
+            events: [
+            {
+                id: '1',
+                title: 'Banana, Apple',
                 start: moment(1484210059985).format(),
                 end: moment(1484223059985).format(),
                 backgroundColor: 'red'
                 // allDay: true
-            }],
-
+            },
+            {
+                id: '1',
+                title: 'feeling',
+                rating: 5,
+                start: moment(1484230059985).format(),
+                end: moment(1484223559985).format(),
+                textColor: 'black'
+            }]
+            
         }
     },
     methods: {
+        setFeelingColor(){
+            let colors = ['','red','red','yellow','yellow','yellow','blue','blue','blue','green','green']
+            this.events.forEach(event => {
+                if(!event['backgroundColor'] && event['title'] === 'feeling'){
+                   event['backgroundColor'] = colors[event['rating']];
+                } 
+                console.log('events', event);
+            })
+            
+            // let color;
 
+            // return color;
+        }
     },
     components: {
         moment,
@@ -25,6 +45,7 @@ export default {
 
     },
     mounted() {
+        this.setFeelingColor();
         $('.calendar').fullCalendar({
             // put your options and callbacks here
 
@@ -41,7 +62,9 @@ export default {
                     titleFormat: 'YYYY, MM, DD'
                 }
             },
-            events: this.meals,
+            events: this.events, 
+       
+
         })
     }
 }
