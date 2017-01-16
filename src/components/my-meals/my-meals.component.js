f814dd619a2bb6efa3b5d7f77509a4b30cfcf550
ResourceBasedEconomy <marky.fridman@gmail.com>
2017-01-16 19:33:10 +0200

    my-meals +feelings in calendar

--- src/components/my-meals/my-meals.component.js
+++ src/components/my-meals/my-meals.component.js
@@ -5,72 +5,77 @@ import { mapGetters, mapActions } from 'vuex';
 export default {
     data: () => {
         return {
-            events: [
-            {
-                id: '1',
-                title: 'Banana, Apple',
-                start: 1484210059985,
-                end: 1484223059985,
-                backgroundColor: '2a3744'
-                // allDay: true
-            },
-            {
-                id: '1',
-                title: 'feeling',
-                rating: 5,
-                start: 1484230059985,
-                end: 1484223559985,
-                textColor: 'black'
-            }]
-            
+            events: [],
+            firstMeals: [],
+            firstFeelings: []
         }
     },
     methods: {
-        setFeelingColor(){
-            let colors = ['','red','red','yellow','yellow','yellow'
-                            ,'blue','blue','blue','green','green']
-            this.events.forEach(event => {
-                if(!event['backgroundColor'] && event['title'] === 'feeling'){
-                   event['backgroundColor'] = colors[event['rating']];
-                } 
-                // console.log('events', event);
-            })
+        setFeelingColor(rating) {
+            let colors = ['', 'red', 'yellow', 'orange', 'blue', 'green'];
+            return colors[rating];
         },
+        translateMeals(meal) {
+            let newMeal = {};
+            newMeal['start'] = meal.time;
+            newMeal['end'] = meal.time - 1600000;
+            newMeal['title'] = meal.foods.join();
+            this.firstMeals.push(newMeal);
+        },
+        translateFeelings(feeling) {
+            let newFeeling = {};
+            newFeeling['title'] = 'Feeling';
+            newFeeling['start'] = feeling.time + 1600000;
+            newFeeling['end'] = feeling.time;
+            newFeeling['backgroundColor'] = this.setFeelingColor(feeling.rating);
+            this.firstFeelings.push(newFeeling);
+        }
+    },
+    mounted() {
+        // this.setFeelingColor();
+        // setTimeout(() => this.events = this.meals);
+        var prmMeals = this.$store.dispatch('getMeals').then(meals => {
+            meals.forEach(meal => {
+                this.translateMeals(meal);
+            });
+        });
+        var prmFeelings = this.$store.dispatch('getFeelings').then((feelings) => {
+            feelings.forEach((feeling) => {
+                this.translateFeelings(feeling)
+            });
+
+        });
+        Promise.all([prmMeals, prmFeelings]).then(values => {
+            this.events = this.firstMeals.concat(this.firstFeelings);
+            console.log(this.events);
+            $('.calendar').fullCalendar({
+                // put your options and callbacks here
+
+                // hiddenDays: [  4, 5,6 ], //choose which days to hide
+                // hiddenDays: [ 0, 1,2,3 ],
+
+                header: { center: 'month, agendaWeek' }, // buttons for switching between views
+                views: {
+                    month: { // name of view
+                        titleFormat: 'YYYY, MM, DD' // name of view
+                        // other view-specific options here
+                    },
+                    agendaWeek: {
+                        titleFormat: 'YYYY, MM, DD'
+                    }
+                },
+                events: this.events,
+            })
+        });
+
+    },
+
+    computed: {
+        ...mapGetters(['meals', 'feelings', 'loading'])
     },
     components: {
         moment,
         FullCalendar,
 
-    },
-    created () {
-        // console.log('dispatching getmeals');
-         this.$store.dispatch('getMeals');
-         
-    },
-    computed  : {
-        ...mapGetters(['meals', 'loading'])
-     },
-    mounted() {
-        this.setFeelingColor();
-        setTimeout(() => this.events = this.meals);
-        $('.calendar').fullCalendar({
-            // put your options and callbacks here
-
-            // hiddenDays: [  4, 5,6 ], //choose which days to hide
-            // hiddenDays: [ 0, 1,2,3 ],
-
-            header: { center: 'month, agendaWeek' }, // buttons for switching between views
-            views: {
-                month: { // name of view
-                    titleFormat: 'YYYY, MM, DD' // name of view
-                    // other view-specific options here
-                },
-                agendaWeek: {
-                    titleFormat: 'YYYY, MM, DD'
-                }
-            },
-            events: this.events,       
-        })
-        console.log('this.meals',  this.events);
     }
 }
