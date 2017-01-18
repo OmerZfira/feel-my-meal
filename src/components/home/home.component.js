65eda983859b95fce57fd679c8b7f0ef6171ef2c
ResourceBasedEconomy <marky.fridman@gmail.com>
2017-01-18 11:52:23 +0200

    filter start

--- src/components/home/home.component.js
+++ src/components/home/home.component.js
@@ -1,7 +1,7 @@
  import addContact from '../add-contact';
  import addMeal from '../add-meal/add-meal';
  import clockMeal from '../utils/clock-meal/clock-meal';
-
+ 
 export default  {
   data: () => {
     return {
