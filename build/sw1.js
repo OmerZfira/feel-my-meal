43f5f1b0ada12024a9f0a9d678b8f70f083d8f0f
ResourceBasedEconomy <marky.fridman@gmail.com>
2017-01-18 12:01:25 +0200

    filter

---
+++ build/sw1.js
@@ -0,0 +1,16 @@
+self.addEventListener('push', function(event) {
+  console.log('[Service Worker] Push Received.');
+  console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
+
+ const title = 'Push Codelab';
+  const options = {
+    body: 'Yay it works.',
+    icon: 'images/icon.png',
+    badge: 'images/badge.png'
+  };
+
+ const notificationPromise = self.registration.showNotification(title, options);
+event.waitUntil(notificationPromise);
+});
+
+
---
+++ src/components/filter/filter.component.vue
@@ -0,0 +1,18 @@
+<template>
+    <h1>Filter Component</h1>
+</template>
+
+<script>
+import {state} from '../../modules/meal/meal.module'
+export default {
+    data() {
+        return {
+
+        }
+    },
+    created() {
+        
+        // console.log('state', state);
+    }
+}
+</script>
