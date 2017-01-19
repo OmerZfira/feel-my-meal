3474d7196fc47eff5d37cddd7a4047cd243ab4e8
Mark Fridman <marky.fridman@gmail.com>
2017-01-19 15:28:45 +0200

    last fix before demo

--- src/components/filter/filter.component.vue
+++ src/components/filter/filter.component.vue
@@ -1,9 +1,9 @@
 <template>
    <section>
-       <div class="form-group">
+
         <label for="usr">Filter Meals</label> 
-        <input class="form-control" type="text" v-model="filterInput" id="usr">
-       </div>
+        <input class="form-control1" type="text" v-model="filterInput" id="usr">
+       
    </section>
 </template>
 
@@ -41,12 +41,49 @@ export default {
 }
 </script>
 
-<style type="scss">
+<style type="scss" >
     h4{
         margin-left: 30px;
+
+        /*TO DO: PUT INPUT INSIDE WRAPPER, (optionally make it flexy), and just give wrapper width*/
     }
     label{
         margin-left: 30px;
     }
+   .form-control1 {
+  display: block;
+  /*width: 100%;*/
+  margin-left: 30px;
+  height: 34px;
+  padding: 6px 12px;
+  font-size: 14px;
+  line-height: 1.42857143;
+  color: #555;
+  background-color: #fff;
+  background-image: none;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  -webkit-box-shadow: inset 0 1px 1px rgba(0, 0, 0, .075);
+          box-shadow: inset 0 1px 1px rgba(0, 0, 0, .075);
+  -webkit-transition: border-color ease-in-out .15s, -webkit-box-shadow ease-in-out .15s;
+       -o-transition: border-color ease-in-out .15s, box-shadow ease-in-out .15s;
+          transition: border-color ease-in-out .15s, box-shadow ease-in-out .15s;
+}
+.form-control1:focus {
+  border-color: #66afe9;
+  outline: 0;
+  -webkit-box-shadow: inset 0 1px 1px rgba(0,0,0,.075), 0 0 8px rgba(102, 175, 233, .6);
+          box-shadow: inset 0 1px 1px rgba(0,0,0,.075), 0 0 8px rgba(102, 175, 233, .6);
+}
+.form-control1::-moz-placeholder {
+  color: #999;
+  opacity: 1;
+}
+.form-control1:-ms-input-placeholder {
+  color: #999;
+}
+.form-control::-webkit-input-placeholder {
+  color: #999;
+}
 
 </style>
