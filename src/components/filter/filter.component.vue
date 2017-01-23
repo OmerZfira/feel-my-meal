39de15659e6f66b5f78bf9a6d69f6f1481039531
Mark Fridman <marky.fridman@gmail.com>
2017-01-23 11:23:34 +0200

    filter

--- src/components/filter/filter.component.vue
+++ src/components/filter/filter.component.vue
@@ -41,7 +41,7 @@ export default {
 }
 </script>
 
-<style type="scss" >
+<style type="scss" scoped>
     h4{
         margin-left: 30px;
 
@@ -51,24 +51,24 @@ export default {
         margin-left: 30px;
     }
    .form-control1 {
-  display: block;
-  /*width: 100%;*/
-  margin-left: 30px;
-  height: 34px;
-  padding: 6px 12px;
-  font-size: 14px;
-  line-height: 1.42857143;
-  color: #555;
-  background-color: #fff;
-  background-image: none;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-  -webkit-box-shadow: inset 0 1px 1px rgba(0, 0, 0, .075);
-          box-shadow: inset 0 1px 1px rgba(0, 0, 0, .075);
-  -webkit-transition: border-color ease-in-out .15s, -webkit-box-shadow ease-in-out .15s;
-       -o-transition: border-color ease-in-out .15s, box-shadow ease-in-out .15s;
-          transition: border-color ease-in-out .15s, box-shadow ease-in-out .15s;
-}
+    display: block;
+    /*width: 100%;*/
+    margin-left: 30px;
+    height: 34px;
+    padding: 6px 12px;
+    font-size: 14px;
+    line-height: 1.42857143;
+    color: #555;
+    background-color: #fff;
+    background-image: none;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    -webkit-box-shadow: inset 0 1px 1px rgba(0, 0, 0, .075);
+            box-shadow: inset 0 1px 1px rgba(0, 0, 0, .075);
+    -webkit-transition: border-color ease-in-out .15s, -webkit-box-shadow ease-in-out .15s;
+        -o-transition: border-color ease-in-out .15s, box-shadow ease-in-out .15s;
+            transition: border-color ease-in-out .15s, box-shadow ease-in-out .15s;
+    }
 .form-control1:focus {
   border-color: #66afe9;
   outline: 0;
