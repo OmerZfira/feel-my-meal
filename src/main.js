877303bf53d05910b585048f059e54418b6d0b6a
omerzfira <omer.zfira@gmail.com>
2017-02-08 23:26:01 +0200

    signup+signin comsmetics

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import './sw-init';
 import Vue from 'vue';
 import moment from 'moment';
 import store from './store';
+import { mapGetters } from 'vuex';
 import router from './routes';
 import MainNav from './components/main-nav';
 import BottomNav from './components/bottom-nav/bottom-nav';
@@ -31,6 +32,11 @@ const app = new Vue({
   data: {
     showModal: false
   },
+    computed: {
+    ...mapGetters({
+      isLoggedIn: 'isLoggedIn',
+    })
+    },
   mounted() {
     let path = (process.env.NODE_ENV === 'development') ? 'http://localhost:8080/add-feeling#/' : 'https://coding-academy.net/feelmymeal/#/add-feeling';
     // let path = 'https://coding-academy.net/feelmymeal/';
